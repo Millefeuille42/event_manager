0bbb2b5cd5da652b5612828367e6fa1eade5c134
Millefeuille42 <mlabouri@student.42.fr>
2022-09-11 22:23:19 +0200

    Added events.routes

--- modules/events_module/routes.py
+++ modules/events_module/routes.py
@@ -1,21 +1,21 @@
 from . import events
+from . import services
+from ..auth_module.guard import login_required
 
 
-@events.route("/events", methods=['Get'])
-def get_events():
-    return 'Hello World!'
+@events.route("/events/campus/<campus_id>", methods=['Get'])
+@login_required
+def get_events(campus_id):
+    return services.get_events_per_campus(campus_id)
 
 
-@events.route("/events/<user_id>/subscribed", methods=['Get'])
+@events.route("/events/user/<user_id>", methods=['Get'])
+@login_required
 def get_subscribed(user_id):
-    return 'Hello World!'
+    return services.get_user_subscribed_events(user_id)
 
 
-@events.route('/events/<user_id>/managed', methods=['Get'])
-def get_managed(user_id):
-    return 'Hello World!'
-
-
-@events.route('/events/<event_id>', methods=['Get'])
+@events.route('/events/id/<event_id>', methods=['Get'])
+@login_required
 def get_id(event_id):
-    return 'Hello World!'
+    return services.get_event_per_id(event_id)
