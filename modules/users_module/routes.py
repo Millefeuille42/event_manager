66c527ec8e16326ef4d4fac959fc1c587b1b168a
Millefeuille <mlabouri@student.42.fr>
2022-09-11 19:11:42 +0200

    Redefined user routes and implemented service

--- modules/users_module/routes.py
+++ modules/users_module/routes.py
@@ -1,24 +1,11 @@
-from __main__ import app
 from . import users
+from .services import get_user_data
+from ..auth_module.guard import login_required
 
 
-@users.route('/users/me', methods=['Get'])
-def get_me():
-    return 'Hello World!'
-
-
-# This route also works for logins
-@users.route('/users/<id>', methods=['Get'])
-def get_subscribed_events():
-    return 'Hello World!'
-
-
-@users.route('/users/<id>', methods=['Post'])
-def get_subscribed_events():
-    return 'Hello World!'
-
-
-@users.route('/users/<id>', methods=['Patch'])
-def get_subscribed_events():
-    return 'Hello World!'
+# This route also works for logins, and "me"
+@users.route('/users/<user_id>/', methods=['Get'])
+@login_required
+def get_user(user_id):
+    return get_user_data(user_id)
 
