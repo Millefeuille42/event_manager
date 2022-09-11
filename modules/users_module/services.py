d1d1d57ee22659540c7d34afa888e239597a33fa
Millefeuille42 <mlabouri@student.42.fr>
2022-09-11 22:22:38 +0200

    Edited users.services for readability

--- modules/users_module/services.py
+++ modules/users_module/services.py
@@ -1,7 +1,7 @@
 import os
-import requests
 from flask import g
-from .interfaces import UserData
+from . import interfaces
+from utils import do_authenticated_request
 
 
 def get_user_data(user_id):
@@ -9,12 +9,7 @@ def get_user_data(user_id):
         target = os.environ['api_url'] + "/v2/" + user_id
     else:
         target = os.environ['api_url'] + "/v2/users/" + user_id
-    print(target)
-    try:
-        response = requests.get(target, headers={'Authorization': 'Bearer ' + g.token})
-        if response.status_code != 200:
-            return {'text': "Error", 'code': response.status_code}
-    except Exception as err:
-        print(err)
-        return {'text': "Unknown Error", 'code': 520}
-    return UserData(response.json()).data
+    data = do_authenticated_request(target, g.token)
+    if data['code'] != 200:
+        return data['text'], data['code']
+    return interfaces.UserData(data['text']).data
