0dd49414cc5c4e8395fdbb69110e414e7c50962e
Millefeuille42 <mlabouri@student.42.fr>
2022-09-11 22:31:51 +0200

    Move token from query to header

--- backend/app/modules/auth_module/guard.py
+++ backend/app/modules/auth_module/guard.py
@@ -6,10 +6,12 @@ from .services import validate_token
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        token = request.args.get("token", default="none")   # Grab token from URI query parameters
+        try:
+            token = request.headers["Authorization"].strip("Bearer ")   # Grab token from URI query parameters
+        except:
+            return "Unauthorized", 401
         token_resp = validate_token(token)  # Check validity of token
         if token_resp['code'] != 200:
-            print(token_resp)
             return "Unauthorized", 401  # If anything goes wrong, deny the request
         return f(*args, **kwargs)       # Else continue request
     return decorated_function
