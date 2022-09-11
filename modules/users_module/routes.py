7cc14a3210881a87efea8da28a7a9f24a307d256
Millefeuille <mlabouri@student.42.fr>
2022-09-11 19:28:09 +0200

    Commented guard use on user route

--- modules/users_module/routes.py
+++ modules/users_module/routes.py
@@ -5,7 +5,7 @@ from ..auth_module.guard import login_required
 
 # This route also works for logins, and "me"
 @users.route('/users/<user_id>/', methods=['Get'])
-@login_required
+@login_required     # This route is protected, check the user token (provided in query parameters for access grant)
 def get_user(user_id):
     return get_user_data(user_id)
 
