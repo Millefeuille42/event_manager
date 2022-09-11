20cec13779657c1df9dc8511da3cceaf7daba95e
Millefeuille42 <mlabouri@student.42.fr>
2022-09-11 22:23:07 +0200

    Added events.services

---
+++ modules/events_module/services.py
@@ -0,0 +1,35 @@
+import os
+from flask import g
+from . import interfaces
+from utils import do_authenticated_request
+
+
+def get_events_per_campus(campus_id):
+    target = os.environ['api_url'] + "/v2/campus/" + campus_id + "/events"
+    print(g.token)
+    data = do_authenticated_request(target, g.token)
+    if data['code'] != 200:
+        return data['text'], data['code']
+    ret = []
+    for event in data['text']:
+        ret.append(interfaces.EventData(event).data)
+    return ret
+
+
+def get_user_subscribed_events(user_id):
+    target = os.environ['api_url'] + "/v2/users/" + user_id + "/events"
+    data = do_authenticated_request(target, g.token)
+    if data['code'] != 200:
+        return data['text'], data['code']
+    ret = []
+    for event in data['text']:
+        ret.append(interfaces.EventData(event).data)
+    return ret
+
+
+def get_event_per_id(event_id):
+    target = os.environ['api_url'] + "/v2/events/" + event_id
+    data = do_authenticated_request(target, g.token)
+    if data['code'] != 200:
+        return data['text'], data['code']
+    return interfaces.EventData(data['text']).data
