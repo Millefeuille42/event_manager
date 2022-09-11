7e98987f7616a5c336d57f88ac76a5fbc6314896
Millefeuille42 <mlabouri@student.42.fr>
2022-09-11 22:19:39 +0200

    Added do_request function to lighten code

---
+++ utils.py
@@ -0,0 +1,13 @@
+import requests
+
+
+def do_authenticated_request(url, token):
+    print("Requesting: " + url)
+    try:
+        response = requests.get(url, headers={'Authorization': 'Bearer ' + token})
+        if response.status_code != 200:
+            return {'text': "Error", 'code': response.status_code}
+    except Exception as err:
+        print(err)
+        return {'text': "Unknown Error", 'code': 520}
+    return {'text': response.json(), 'code': 200}
