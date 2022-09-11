1eb093d9eebb8e95fed7713048d5dd8996e738c1
Millefeuille <mlabouri@student.42.fr>
2022-09-11 19:12:35 +0200

    Added auth and user module

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ import os
 from flask import Flask
 from modules.events_module import events
 from modules.users_module import users
+from modules.auth_module import auth
 
 app = Flask(__name__)
 
 app.register_blueprint(events)
+app.register_blueprint(users)
+app.register_blueprint(auth)
 
 if __name__ == '__main__':
     app.run(os.environ['back_host'], int(os.environ['back_port']), debug=True)
