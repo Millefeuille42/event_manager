f561ea6f53ef3c2af7caaadcd87714b25a869c2e
Millefeuille42 <mlabouri@student.42.fr>
2022-09-12 02:05:49 +0200

    Added CORS management

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,10 +1,27 @@
 import os
-from flask import Flask
+from flask import Flask, request
 from modules.events_module import events
 from modules.users_module import users
 from modules.auth_module import auth
 
 app = Flask(__name__)
+
+
+@app.before_request
+def before_request():
+    print("Test")
+    if request.method == "OPTIONS":
+        return "OK", 200
+
+
+@app.after_request
+def after_request(response):
+    header = response.headers
+    header['Access-Control-Allow-Origin'] = '*'
+    header['Access-Control-Allow-Methods'] = "GET, OPTIONS"
+    header['Access-Control-Allow-Headers'] = 'authorization'
+
+    return response
 
 # Import all routes blueprint from modules
 app.register_blueprint(events)
